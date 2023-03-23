6d22dd3dc54ec5d3318b88e5ae88cb32517d007a
ol-s <marthablue.256@gmail.com>
2023-03-22 22:31:27 -0300

    json y proyectos dividos proyArq proyWeb

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
 
+  proyectosTodos : any = [];
+  proyectosTodosWeb : any = [];
+
+  //datos: tambien es un alias, nombrar como querramos
+  constructor(private datos: DatosService) { }
+
+  //this 'datos' es el datos de la linea 15, el getDatos viene del datos.service.ts,  'data'  es otro alias que ponemos nosotros, puse los mismos alias que en educacion.ts
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data => {
+      this.proyectosTodos = data.proyectosArq,
+      this.proyectosTodosWeb = data.proyectosWeb
+    });
+  }
 }
+
