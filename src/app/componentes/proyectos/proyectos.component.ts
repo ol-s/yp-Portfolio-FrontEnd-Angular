cab48062606b41bf30d4f06020e1c922a65a289c
ol-s <marthablue.256@gmail.com>
2023-03-22 23:02:43 -0300

    proyectos unificados en json y html, icon +

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -8,15 +8,19 @@ import { DatosService } from 'src/app/servicios/datos.service';
 })
 export class ProyectosComponent implements OnInit {
 
+  //proyectosTodos serian los primeros que separe como arq, luego agregue los web, pero despues los puse todos en el mismo array del json 
+  //con href (+) img para los arq y href github para los web, maybe poner la urlde pagina final
   proyectosTodos : any = [];
   proyectosTodosWeb : any = [];
 
   //datos: tambien es un alias, nombrar como querramos
   constructor(private datos: DatosService) { }
 
-  //this 'datos' es el datos de la linea 15, el getDatos viene del datos.service.ts,  'data'  es otro alias que ponemos nosotros, puse los mismos alias que en educacion.ts
+  //this 'datos' es el datos de la linea 17, el getDatos viene del datos.service.ts,  'data'  es otro alias que ponemos nosotros, puse los mismos alias que en educacion.ts
   ngOnInit(): void {
     this.datos.getDatos().subscribe(data => {
+
+      //------------------si saco 'proyectosArq' de json cambiar tmb aca--------------------------
       this.proyectosTodos = data.proyectosArq,
       this.proyectosTodosWeb = data.proyectosWeb
     });
