f995e5e6ba134dcd7af647d7d0858b0e775ffb5c
ol-s <marthablue.256@gmail.com>
2023-03-23 00:15:36 -0300

    skills ts, antes de tocar skills html

--- src/app/componentes/skills/skills.component.ts
+++ src/app/componentes/skills/skills.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatosService } from 'src/app/servicios/datos.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
 
+export class SkillsComponent implements OnInit {
+
+  //no se si dividir cada rubro skill aca tmb
+  //proyectosTodos : any = [];
+  //proyectosTodosWeb : any = [];
+
+  skillsTodas: any = [];
+
+  //datos: tambien es un alias, nombrar como querramos
+  constructor(private datos: DatosService) { }
+
+  //this 'datos' es el datos de la linea 14, el getDatos viene del datos.service.ts,  'data'  es otro alias que ponemos nosotros, puse los mismos alias que en educacion.ts
+  ngOnInit(): void {
+    this.datos.getDatos().subscribe(data => {
+        this.skillsTodas = data.skillsProgramacion,
+        this.skillsTodas = data.skillsDisenioArquitectura,
+        this.skillsTodas = data.skillsIdiomas,
+        this.skillsTodas = data.skillsHabilidadesBlandas
+    });
+  }
 }
+
