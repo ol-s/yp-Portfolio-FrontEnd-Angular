742dfae5a518820e27ff93d53b60742424f4d22c
ol-s <marthablue.256@gmail.com>
2023-04-04 22:54:27 -0300

    model 1 class

---
+++ src/app/model/educacion.ts
@@ -0,0 +1,24 @@
+export class Educacion {
+
+    id?: number;                //signo ! o ? si da error //si tengo fechas tmb pongo string, el back lo convierte a date
+    logoInstitucion: string;
+    logoAlt: string;
+    anioeInstitucion: string;
+    titulo: string;
+    descripcion: string;
+
+
+    constructor(
+        logoInstitucion: string,
+        logoAlt: string,
+        anioeInstitucion: string,
+        titulo: string,
+        descripcion: string) {
+
+        this.logoInstitucion = logoInstitucion;  //el primero es el de arriba, el segundo el del constructor
+        this.logoAlt = logoAlt;
+        this.anioeInstitucion = anioeInstitucion;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+}
