3cb5b617d2e1a466d11ce9fe63854753f1158355
ol-s <marthablue.256@gmail.com>
2023-04-05 13:15:18 -0300

    5-4-23 educacion conect a bd, edited from phpmyadm

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,5 +1,11 @@
+///////////////////////////////////////////////////integrado al back, desde 5.4.23///////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////EDITADO EDUCACION DESDE PHPMYADMIN!!////////////////////////////////////
+
 import { Component, OnInit } from '@angular/core';
-import { DatosService } from 'src/app/servicios/datos.service';
+//import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Educacion } from 'src/app/model/educacion';                          //*** 
+import { EducacionService } from 'src/app/servicios/educacion.service';
 
 @Component({
   selector: 'app-educacion',
@@ -7,32 +13,123 @@ import { DatosService } from 'src/app/servicios/datos.service';
   styleUrls: ['./educacion.component.css']
 })
 
-export class EducacionComponent implements OnInit {       //con oninit me tira error, faltaba import oninit en linea 1               
-  //export class EducacionComponent {
+export class EducacionComponent implements OnInit {          
 
-  //instanciamos la variable, para traer un array completo [], 'any'
-  estudiosTodos: any = [];
-  //estudios: Estudio[]=[];    para llamar de a uno?  'se llama al modelo que es un array'
+  //isLogged:boolean=false;  //h
+  estudiosTodos: Educacion[]=[]; //se llama al modelo (poner el nombre que lleva en model, Educacion ***) que es un array
+  bannerEducacion: string = '';  //como voy a traer los banners?? :/
+  titulo: string = "Educacion";  //k
 
-  //para traer un solo dato
-  nombreCompleto: string = '';
+  //serviEduc es mi alias
+  constructor(private serviEduc: EducacionService) { }
 
-  //banner background fixed
-  bannerEducacion: string = '';
+  
+  // ngOnInit(): void {
+  //   this.datos.getDatos().subscribe(data => {
+  //     this.estudiosTodos = data.estudios,
+  //       this.nombreCompleto = data.nombreyapellido,   //si quisieramos traer solo el nombre
+  //       this.bannerEducacion = `url(${data.bannerEducacion})`   //background imagen
+  //   });
 
-  //datos: tambien es un alias, nombrar como querramos
-  constructor(private datos: DatosService) { }
+  // }
 
-  //this 'datos' es el datos de la linea 21, el getDatos viene del datos.service.ts,  'data'  es otro alias que ponemos nosotros
-  ngOnInit(): void {
-    this.datos.getDatos().subscribe(data => {
-      this.estudiosTodos = data.estudios,
-        this.nombreCompleto = data.nombreyapellido,   //si quisieramos traer solo el nombre
-        this.bannerEducacion = `url(${data.bannerEducacion})`   //background imagen
-    });
 
+  ngOnInit(): void {  //no va nada aca adentro?, AH, DESPUES DEL METODO ABAJO CARGAMOS ACA ARRIBA //'ngonnit' cos se carga al inicio, no hagas nada, solamente mostrame esto
+    this.cargarEstudios();
+ 
+
+  }   
+
+  
+  //delante puede ir public o private, si no tiene nada es public
+  //cargarestudios en mi metodo, de tipovoid porque no retorna nada,,,
+  //this. es para utilizar una variable que esta fuera del metodo, si le pasara id entre los parenteiss, abajo no pondria 'this.' ,,,express 18, min 38'
+  
+
+  //solo este metodo hacemos?? y el new/find/update/delete?  h nor k  lo tienen...  k no tiene delete tampoco
+  cargarEstudios():void{
+    this.serviEduc.listaEstudios().subscribe(data => {this.estudiosTodos=data});  
   }
+  //listaEstudios()  es lo mismo que escribi en el educacion.service.ts,,,,si tuviera algo entre parentsis aca lo pongo 
+  //'data' es alias
+  //estudiosTodos es la variable de mas arriba  estudiosTodos: Educacion[]=[]
 
 
 
+
+
+  //en express h tenia este tmb, no en ghb, me da error aca
+  // idEdit(id: number) {
+  //   this.isTrue = true;
+  //   this.isEditar = id;
+  //   }
+
+  deleteEstudio(id: number){  //ESTO NO USE, VER SI QUEDA DESPUES
+    if(id != undefined){
+      this.serviEduc.deleteEstudio(id).subscribe(data =>{
+          alert("Estudio eliminado")
+          this.cargarEstudios();
+        }, err =>{
+          alert("No se pudo eliminar el estudio")
+        })
+    }}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////////CORRIENDO OK CON JSON ANGULAR HASTA 4.4.23/////////////////////////
+////////////////////si comento lo de arriba y descomento esto se ve como antes//////////////////////////////////////////////////
+
+
+
+/*
+ import { Component, OnInit } from '@angular/core';
+ import { DatosService } from 'src/app/servicios/datos.service';
+
+ @Component({
+   selector: 'app-educacion',
+   templateUrl: './educacion.component.html',
+   styleUrls: ['./educacion.component.css']
+ })
+
+ export class EducacionComponent implements OnInit {       //con oninit me tira error, faltaba import oninit en linea 1               
+   //export class EducacionComponent {
+
+   //instanciamos la variable, para traer un array completo [], 'any'
+   estudiosTodos: any = [];
+   //estudios: Estudio[]=[];    para llamar de a uno?  'se llama al modelo que es un array'
+
+   //para traer un solo dato
+   nombreCompleto: string = '';
+
+   //banner background fixed
+   bannerEducacion: string = '';
+
+   //datos: tambien es un alias, nombrar como querramos
+   constructor(private datos: DatosService) { }
+
+   //this 'datos' es el datos de la linea 21, el getDatos viene del datos.service.ts,  'data'  es otro alias que ponemos nosotros
+   ngOnInit(): void {
+     this.datos.getDatos().subscribe(data => {
+       this.estudiosTodos = data.estudios,
+         this.nombreCompleto = data.nombreyapellido,   //si quisieramos traer solo el nombre
+         this.bannerEducacion = `url(${data.bannerEducacion})`   //background imagen
+     });
+
+   }
+
+ }
+
+
+
+
+*/
--- src/app/servicios/educacion.service.ts
+++ src/app/servicios/educacion.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Educacion } from '../model/educacion';
+import { Experiencia } from '../model/experiencia';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ import { Educacion } from '../model/educacion';
 
 export class EducacionService {
                                                                                      //barra llama a un pedacito de json,como los pipes en html?
-  url:string= "/servidor/educacion/";      //"http://localhost:8080/educacion"        //barra al final asi no va en cada uno abajo
+  url:string= "http://localhost:8080/educacion/";      //"http://localhost:8080/educacion"        //barra al final asi no va en cada uno abajo
   
   constructor(private httpClient:HttpClient) { }      //primer httpclient es alias, segundo lo importamos arriba, httpclient es herramienta para hacer el crud                                
                                                       
@@ -31,18 +32,83 @@ export class EducacionService {
       return this.httpClient.post<any>(this.url + 'new', Educacion);
       }
 
-    //igual al de arriba, uno es post, otro put, por convencion, podrian ser los dos post (?)  expres 18, 22, 20'
+    //igual al de arriba, uno es post, otro put, por convencion, podrian ser los dos post (?)  expres 18, 22:20'
     //''no se va a hacer una nueva experiencia porque vamos a mandar el id dentro del formulario,vamos a sobreescribir,,
     //ESE ES EL SECRETO DE QUE EL UPDATE ANDE BIEN'' HHHHHHHHMMMMMM AHHHHHHHHHHHH
     public updateEstudio(Educacion: Educacion):Observable<any>{
       return this.httpClient.put<any>(this.url + 'update', Educacion);
       }
   
+    //ID: NUMBER, PORQUE ES TYPESCRIPT, hay q decirle de que tipo es, <any> borra todos los campos q tenga ese id
     public deleteEstudio(id: number):Observable<any>{
       return this.httpClient.delete<any>(this.url + `delete/${id}`);
       }
 
-  // min30'  , corto
-  //https://www.youtube.com/watch?v=9wUALiL0UKA&list=PL1oXSbt2OIbG2ipcgJLQ8P64rwS-bz9tZ
 
 }
+
+
+
+
+
+
+
+
+//editar por id seria, CREO, IGUAL TENDRIA QUE CAMBIAR EN EL NETB TOO
+//  public updateEstudio(id: number, experiencia: Experiencia):Observable<any>{
+//       return this.httpClient.put<any>(this.url + `update/${id}`, experiencia);
+//       }
+
+// en netb
+// //@RequestBody Educacion edu)
+// //request body sacar? 'por resquest body o por objeto, so deja Educacion
+// @putMapping ("/update/{id}")
+// public vouid update (@PathVariable ("id") int id,Educacion edu){
+// sEducacion.save(edu);
+// }  
+// //////este no era el que no me andaba en postman??? q volvia null null null?
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////lo mismo con coments
+
+// export class EducacionService {
+//                                                                                      //barra llama a un pedacito de json,como los pipes en html?
+//   url:string= "/servidor/educacion/";      //"http://localhost:8080/educacion"        //barra al final asi no va en cada uno abajo
+  
+//   constructor(private httpClient:HttpClient) { }      //primer httpclient es alias, segundo lo importamos arriba, httpclient es herramienta para hacer el crud                                
+                                                      
+//     public listaEstudios(): Observable<Educacion[]>{                     ////////va el nombre del controller 1er o 2 renglon?????!!!!!
+//       return this.httpClient.get<Educacion[]>(this.url + 'lista');          /////con los tengo diferentes, va el de arriba CREO
+//     }                                                                       ////salen del CEducacion del back en java 
+                                                                          
+//     // METODO (nombre igual que en el controller en java, public listaEstudios()  ): observable <me trae un array []>---ABAJO ES UNA SOLA BY ID!
+//     //httpclient will GET <todo el array de educacion[]>(esta url escrita arriba +'lista' EN COMILLAS SIMPLES, OJO)
+
+
+//     public findEstudio(id: number):Observable<Educacion>{
+//     return this.httpClient.get<Educacion>(this.url + `find/${id}`);           //los: lista/find/new/update/delete, como los describi en java
+//     }                                                                        //localhost:8080/educacion/find/${id}       es seria la ruta completa
+  
+//     //primer Educacion es un alias, segundo es el Educacion del model, o sea la entidad Educacion! (?)
+//     //observable de any ................etc.......'new', Educacion (del alias del primer Educacion, si clikeas te marca ese y el otro)
+//     public saveEducacion(Educacion: Educacion):Observable<any>{
+//       return this.httpClient.post<any>(this.url + 'new', Educacion);
+//       }
+
+//     //igual al de arriba, uno es post, otro put, por convencion, podrian ser los dos post (?)  expres 18, 22:20'
+//     //''no se va a hacer una nueva experiencia porque vamos a mandar el id dentro del formulario,vamos a sobreescribir,,
+//     //ESE ES EL SECRETO DE QUE EL UPDATE ANDE BIEN'' HHHHHHHHMMMMMM AHHHHHHHHHHHH
+//     public updateEstudio(Educacion: Educacion):Observable<any>{
+//       return this.httpClient.put<any>(this.url + 'update', Educacion);
+//       }
+  
+//     //ID: NUMBER, PORQUE ES TYPESCRIPT, hay q decirle de que tipo es, <any> borra todos los campos q tenga ese id
+//     public deleteEstudio(id: number):Observable<any>{
+//       return this.httpClient.delete<any>(this.url + `delete/${id}`);
+//       }
+
+
+// }
