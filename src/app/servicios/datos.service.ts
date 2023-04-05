f2305ed17b484035f86d4ae20bd450e626a1cfae
ol-s <marthablue.256@gmail.com>
2023-04-04 21:16:51 -0300

    angular2 integracion con back

--- src/app/servicios/datos.service.ts
+++ src/app/servicios/datos.service.ts
@@ -21,4 +21,5 @@ export class DatosService {
     //retorna datos via get httcliente que llama a la base de datos json o a una url , get('usar comillas simples')
     return this.httpCliente.get('./assets/basedatos/database.json');
   }
+  //no me dice nada elas viias, esta en github o no??
 }
